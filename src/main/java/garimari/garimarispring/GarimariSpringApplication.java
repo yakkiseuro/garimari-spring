4caed3221a1aebcdc735694bc4eb34a4bc841aef
hoon <easthoon1996@gmail.com>
2022-03-14 17:32:27 +0900

    - test

---
+++ src/main/java/garimari/garimarispring/GarimariSpringApplication.java
@@ -0,0 +1,13 @@
+package garimari.garimarispring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarimariSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GarimariSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/garimari/garimarispring/GarimariSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package garimari.garimarispring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GarimariSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
